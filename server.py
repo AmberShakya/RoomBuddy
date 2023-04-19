83f183ad3a3d450e60f9cb8cde422012f43add75
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-04-19 12:42:18 +0530

    update

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
+from wsgiref.simple_server import make_server
+from flask import Flask, jsonify, make_response, render_template, request, request_started
+from dotenv import load_dotenv
+load_dotenv()
+app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('APP_PORT')
