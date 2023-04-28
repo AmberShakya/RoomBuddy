df48167e86982cc944ebc56cb153ec1aa8e1ee38
nishaanth <nishaanth@Nishaanths-MacBook-Air.local>
2023-04-28 23:45:28 +0530

    score page fix

--- cli/score_page.cpp
+++ cli/score_page.cpp
@@ -11,13 +11,13 @@ private:
     int rollno;
     string dept;
     string email;
-    // int phoneNum;
-    vector<string> interests;
+    string interests;
     int score;
+    bool available;
 
 public:
-    User(string name, bool gender, int rollno, string dept, string email, int phoneNum, vector<string> interests)
-        : name(name), gender(gender), rollno(rollno), dept(dept), email(email), phoneNum(phoneNum), interests(interests), score(0) {}
+    User(string name, bool gender, int rollno, string dept, string email, string interests, int score, bool available)
+        : name(name), gender(gender), rollno(rollno), dept(dept), email(email), interests(interests), score(score), available(available) {}
 
     void addScore(int points)
     {
@@ -29,7 +29,7 @@ public:
         return score;
     }
 
-    vector<string> getInterests()
+    string getInterests()
     {
         return interests;
     }
@@ -40,21 +40,21 @@ public:
     }
 };
 
-void scoreUsers(vector<User> &userList)
+void scoreUsers(vector<User>& userList)
 {
-    vector<pair<int, string>> scoreList; // vector to store scores and corresponding user name
+    vector<pair<int, string> > scoreList; // vector to store scores and corresponding user name
 
-    for (int i = 0; i < userList.size(); i++) // iterate over all users
-    {
+    int i=0; // iterate over all users
+    
         for (int j = i + 1; j < userList.size(); j++) // compare with all other users after i
         {
             int commonInterests = 0;
-            vector<string> iInterests = userList[i].getInterests();
-            vector<string> jInterests = userList[j].getInterests();
+            string iInterests = userList[i].getInterests();
+            string jInterests = userList[j].getInterests();
 
-            for (int k = 0; k < iInterests.size(); k++) // count number of common interests
+            for (int k = 0; k < iInterests.length() && k < jInterests.length(); k++) // count number of common interests
             {
-                if (find(jInterests.begin(), jInterests.end(), iInterests[k]) != jInterests.end())
+                if (iInterests[k] == '1' && jInterests[k] == '1')
                 {
                     commonInterests++;
                 }
@@ -67,9 +67,8 @@ void scoreUsers(vector<User> &userList)
                 scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + to_string(userList[j].getScore()) + ")")); // add other user's name and score to vector
             }
         }
-    }
 
-    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string>>()); // sort vector in descending order of score
+    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string> >()); // sort vector in descending order of score
 
     for (int i = 0; i < scoreList.size(); i++) // print results
     {
@@ -77,29 +76,21 @@ void scoreUsers(vector<User> &userList)
     }
 }
 
+
+
 int main()
 {
     vector<User> userList;
 
     // populate userList with data
 
-    // example data
-    vector<string> interests1 = {"Cricket", "Football", "Hockey"};
-    vector<string> interests2 = {"Cricket", "Tennis"};
-    vector<string> interests3 = {"Football", "Basketball"};
-    vector<string> interests4 = {"Cricket", "Hockey", "Basketball"};
-    vector<string> interests5 = {"Hockey", "Tennis"};
+    User user3("nishaanth",1,3, "EC", "nitin@example.com", "1100101000", 0, true);
+    User user4("David",0,4, "CS", "david@example.com", "1111101110", 0, true);
+    User user5("Emily",1,5, "ME", "emily@example.com", "1100001101", 0, true);
 
-    User user1("Alice", 0, 1, "CS", "alice@example.com", 1234567890, interests1);
-    User user2("Bob", 1, 2, "EC", "bob@example.com", 2345678901, interests2);
-    User user3("Charlie", 1, 3, "ME", "charlie@example.com", 3456789012, interests3);
-    User user4("Eve", 0, 5, "EE", "eve@example.com", 5678901234, interests5);
-
-    userList.push_back(user1);
-    userList.push_back(user2);
     userList.push_back(user3);
     userList.push_back(user4);
-
+    userList.push_back(user5);
     scoreUsers(userList);
 
     return 0;
--- cli/test.cpp
+++ cli/test.cpp
@@ -1,1 +1,10 @@
 //nothing
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a ="10110";
+    string b ="10100";
+
+    
+}
