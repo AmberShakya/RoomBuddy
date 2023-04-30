17753488e7e5ab87220b8f7791d9677439804ac4
sanjiv kannaa jeganathan <84980036+SanjivKannaa@users.noreply.github.com>
2023-04-30 19:51:14 +0530

    Update hashing.cpp

--- cli/hashing.cpp
+++ cli/hashing.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-
-unsigned int simple_hash(const std::string& str) {
-    unsigned int hash = 5381;
-    for (char c : str) {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return hash;
-}
+#include <string>
+#include <functional>
 
 int main() {
-    std::string message;
-    std::cin >> message;
-    unsigned int hash = simple_hash(message);
-    std::cout << "Hash of '" << message << "': " << hash << std::endl;
+    std::string str;
+    std::cin >> str;
+    std::hash<std::string> hash_fn; // create a hash function object for std::string
+    size_t hash_value = hash_fn(str); // generate hash value for the string
+    std::cout << "Hash value of \"" << str << "\" is: " << hash_value << std::endl;
     return 0;
 }
-
