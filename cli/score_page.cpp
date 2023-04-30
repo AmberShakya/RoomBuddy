f7f2874ef2c44f473b2669a16b1eed507c0f0377
nishaanth <nishaanth@Nishaanths-MacBook-Air.local>
2023-04-30 18:58:49 +0530

    added testing file

--- cli/score_page.cpp
+++ cli/score_page.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include<sqlite3.h>
 using namespace std;
 
 class User
@@ -54,20 +55,61 @@ public:
         return email;
     }
 
-    bool isAvailable() // public member function to access the 'available' variable
+    bool isAvailable() 
     {
         return available;
     }
 
-    bool hasSameGender(User otherUser) // public member function to access the 'gender' variable
+    bool hasSameGender(User otherUser) 
     {
         return gender == otherUser.gender;
     }
 };
 
+vector<User> getUsersFromDatabase(int rollno)
+{
+    vector<User> userList;
+
+    // connect to database
+    sqlite3* db;
+    if (sqlite3_open("students.db", &db) == SQLITE_OK)
+    {
+        // build query to retrieve user details
+        string query = "SELECT name, gender, rollno, dept, email, interests, score, available FROM Students WHERE rollno != " + to_string(rollno) + ";";
+
+        // execute query and retrieve results
+        sqlite3_stmt* stmt;
+        if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+        {
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                // retrieve values from database row
+                string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+                bool gender = sqlite3_column_int(stmt, 1);
+                int rollno = sqlite3_column_int(stmt, 2);
+                string dept = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
+                string email = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4));
+                string interests = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 5));
+                int score = sqlite3_column_int(stmt, 6);
+                bool available = sqlite3_column_int(stmt, 7);
+
+                // create User object and add to vector
+                User user(name, gender, rollno, dept, email, interests, score, available);
+                userList.push_back(user);
+            }
+        }
+
+        // finalize statement and close database
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+    }
+
+    return userList;
+}
+
 void scoreUsers(vector<User> &userList)
 {
-    vector<pair<int, string>> scoreList; // vector to store scores and corresponding user name
+    vector<pair<int, string> > scoreList; // vector to store scores and corresponding user name
 
     int i = 0;
 
@@ -91,14 +133,14 @@ void scoreUsers(vector<User> &userList)
             {
                 userList[i].addScore(commonInterests);
                 userList[j].addScore(commonInterests);
-                scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + to_string(userList[j].getScore()) + ")")); // add other user's name and score to vector
+                scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + userList[j].getEmail() + ", " + to_string(userList[j].getScore()) + ")")); // add other user's name, email, and score to vector
+            }
             }
         }
-    }
 
-    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string>>()); // sort vector in descending order of score
+    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string> >()); // sort vector in descending order of score
 
-    for (int i = 0; i < scoreList.size(); i++) // print results
+    for(int i = 0; i < scoreList.size(); i++) // print results
     {
         cout << scoreList[i].second << endl;
     }
