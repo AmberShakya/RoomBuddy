de55a6957379015f3ac9c6ae97068df7e685b155
nithin-kasauzki <106121086@nitt.edu>
2023-04-30 22:47:29 +0530

    all done

--- cli/score_page.cpp
+++ cli/score_page.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include<sqlite3.h>
-#include<tuple>
+#include <sqlite3.h>
+#include <tuple>
 using namespace std;
 
 class User
@@ -56,16 +56,17 @@ public:
         return email;
     }
 
-    bool isAvailable() 
+    bool isAvailable()
     {
         return available;
     }
 
-    bool hasSameGender(User otherUser) 
+    bool hasSameGender(User otherUser)
     {
         return gender == otherUser.gender;
     }
-    bool getGender(){
+    bool getGender()
+    {
         return gender;
     }
 };
@@ -76,21 +77,21 @@ pair<string, bool> getUserInfoFromDatabase(int rollno)
     bool gender;
 
     // connect to database
-    sqlite3* db;
+    sqlite3 *db;
     if (sqlite3_open("student.db", &db) == SQLITE_OK)
     {
         // build query to retrieve user details
         string query = "SELECT gender, interest FROM Students WHERE rollno = " + to_string(rollno);
 
         // execute query and retrieve results
-        sqlite3_stmt* stmt;
+        sqlite3_stmt *stmt;
         if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) == SQLITE_OK)
         {
             if (sqlite3_step(stmt) == SQLITE_ROW)
             {
                 // retrieve values from database row
                 gender = sqlite3_column_int(stmt, 0);
-                const char* interestsChars = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+                const char *interestsChars = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
                 interests = interestsChars ? interestsChars : "";
                 interests.resize(strlen(interestsChars));
             }
@@ -119,31 +120,30 @@ pair<string, bool> getUserInfoFromDatabase(int rollno)
     return make_pair(interests, gender);
 }
 
-
 vector<User> getUsersFromDatabase()
 {
     vector<User> userList;
 
     // connect to database
-    sqlite3* db;
+    sqlite3 *db;
     if (sqlite3_open("student.db", &db) == SQLITE_OK)
     {
         // build query to retrieve user details
         string query = "SELECT name, gender, rollno, dept, email, interest, available FROM Students";
 
         // execute query and retrieve results
-        sqlite3_stmt* stmt;
+        sqlite3_stmt *stmt;
         if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) == SQLITE_OK)
         {
             while (sqlite3_step(stmt) == SQLITE_ROW)
             {
                 // retrieve values from database row
-                string name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
+                string name = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
                 int gender = sqlite3_column_int(stmt, 1);
                 int rollno = sqlite3_column_int(stmt, 2);
-                string dept = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
-                string email = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4));
-                string interests = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 5));
+                string dept = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3));
+                string email = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 4));
+                string interests = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 5));
                 int available = sqlite3_column_int(stmt, 6);
 
                 // create User object with score = 0 and add to vector
@@ -170,32 +170,30 @@ vector<User> getUsersFromDatabase()
     return userList;
 }
 
-
-
-
-//function to get the interest,gender of a user from database by giing rollnum
-void scoreUsers(vector<User> userList , int roll)
+// function to get the interest,gender of a user from database by giing rollnum
+void scoreUsers(vector<User> userList, int roll)
 {
-    vector<pair<int, string> > scoreList; // vector to store scores and corresponding user name
+    vector<pair<int, string>> scoreList; // vector to store scores and corresponding user name
     string iInterests;
     int iGender;
     // Get user info for the specified roll number
     pair<string, int> user_info = getUserInfoFromDatabase(roll);
     iInterests = get<0>(user_info);
-    iGender =get<1>(user_info);
-    
+    iGender = get<1>(user_info);
+
     for (int j = 0; j < userList.size(); j++) // compare with all other users after i
     {
-        
+
         if (iGender == userList[j].getGender() && userList[j].isAvailable())
         { // check if users have same gender and are available
             int commonInterests = 0;
             string jInterests = userList[j].getUserInterests();
             for (int k = 0; k < 22; k++) // count number of common interests
-            {   cout<<"\nim in";
+            {
+                cout << "\nim in";
                 if (iInterests[k] == '1' && jInterests[k] == '1')
                 {
-                    cout<<"\t"<<commonInterests;
+                    cout << "\t" << commonInterests;
                     commonInterests = commonInterests + 1;
                 }
             }
@@ -207,28 +205,28 @@ void scoreUsers(vector<User> userList , int roll)
         }
     }
 
-    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string> >()); // sort vector in descending order of score
+    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string>>()); // sort vector in descending order of score
 
-    for(int i = 0; i < scoreList.size(); i++) // print results
+    for (int i = 0; i < scoreList.size(); i++) // print results
     {
         cout << scoreList[i].second << endl;
     }
 }
 
-
 int main()
 {
-vector<User> userList;
-string iInterest;
-bool iGender;
-int rollnum = 9; // example rollnum
+    // vector<User> userList;
+    string iInterest;
+    bool iGender;
+    int rollnum = 9; // example rollnum
 
-
-    userList = getUsersFromDatabase();
-
-    scoreUsers(userList,rollnum);
-    //cout << userList[0].getUserInterests() << endl;
-
+    // cout << userList[0].getUserInterests() << endl;
 
     return 0;
+}
+
+void viewMatches(int rollnum)
+{
+    vector<User> userList = getUsersFromDatabase();
+    scoreUsers(userList, rollnum);
 }
--- main/main.cpp
+++ main/main.cpp
@@ -6,6 +6,8 @@
 #include <iomanip>
 #include <algorithm>
 #include <bitset>
+#include <unistd.h>
+#include <fstream>
 // #include <openssl/sha.h>
 // install from https://www.openssl.org/source/
 //  #include <openssl/evp.h>
@@ -71,19 +73,37 @@ public:
     {
         return gender == otherUser.gender;
     }
+    bool getGender()
+    {
+        return gender;
+    }
 };
 
 string hobies[] = {"basketball", "volleyball", "football", "swimming", "running", "cycling", "cricket", "chess", "Music", "Singing", "AI & machine learning", "web & app development", "computer programming", "Blockchain", "Video Games", "Traveling and exploring new places", "painting", "drawing", "photography", "graphic design", "Watching movies", "Watching series"};
 bool authenticate(int rollno, const string password);
 int login();
 void dashboard(int rollno);
+
+void myAccount(int rno);
 string getInterest();
 int getNewUserData();
 void initialScreen();
+
 int insertStudentData(const string name, bool gender, int rollno, const string dept,
                       const string email, const string password, const string interest, bool available);
 bool isRollNoExists(int rollno);
-
+void updateInterest(int rollnum, string interests);
+string hash_password(string password);
+void scoreUsers(vector<User> userList, int roll);
+vector<User> getUsersFromDatabase();
+pair<string, bool> getUserInfoFromDatabase(int rollno);
+void viewMatches(int rollnum);
+//
+//
+//
+//
+//
+//
 bool authenticate(int rollno, const string password)
 {
     sqlite3 *db;
@@ -140,6 +160,7 @@ int login()
 
     cout << "Password: ";
     cin >> password;
+    password = hash_password(password);
     if (isRollNoExists(rollno))
     {
         if (authenticate(rollno, password))
@@ -159,6 +180,65 @@ int login()
         return 0;
     }
     return rollno;
+}
+
+string hash_password(string password)
+{
+    unsigned int hash_value = 0;
+    for (size_t i = 0; i < password.length(); i++)
+    {
+        hash_value += password[i];
+    }
+    string hash_string = "";
+    char hex_digits[] = "0123456789abcdef";
+    while (hash_value > 0)
+    {
+        int digit = hash_value % 16;
+        hash_string = hex_digits[digit] + hash_string;
+        hash_value /= 16;
+    }
+    transform(hash_string.begin(), hash_string.end(), hash_string.begin(), ::tolower);
+    return hash_string;
+}
+void updateInterest(int rollnum, string interests)
+{
+    sqlite3 *db;
+    int rc = sqlite3_open("student.db", &db); // student.db is the name of the database file
+
+    if (rc)
+    {
+        sqlite3_close(db);
+        cout << "Failed to open database" << endl;
+        return;
+    }
+
+    const char *sql = "UPDATE Students SET Interest = ? WHERE RollNo = ?";
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK)
+    {
+        sqlite3_close(db);
+        cout << "Failed to prepare statement" << endl;
+        return;
+    }
+
+    sqlite3_bind_text(stmt, 1, interests.c_str(), -1, SQLITE_TRANSIENT);
+    sqlite3_bind_int(stmt, 2, rollnum);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE)
+    {
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        cout << "Failed to execute statement" << endl;
+        return;
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    cout << "Interests updated successfully" << endl;
+    sleep(1);
 }
 
 string getInterest()
@@ -312,7 +392,7 @@ int getNewUserData()
     string email;
     int phoneNum;
     string interest = "";
-    bool available = false;
+    bool available = true;
 
     cout << "\nName: ";
     cin >> name;
@@ -374,7 +454,7 @@ int getNewUserData()
     } while (password != password_confirmation);
 
     cout << "Password confirmed!" << endl;
-
+    password = hash_password(password);
     cout << "*********************************************************";
 
 #ifdef _WIN32 // to clear the terminal screen
@@ -387,11 +467,13 @@ int getNewUserData()
     if (insertStudentData(name, gender, rollno, dept, email, password, interest, available) != 0)
     {
         cout << "\nsuccessfully inserted";
+        sleep(1);
         return rollno;
     }
     else
     {
         cout << "\nFailed to insert";
+        sleep(1);
         return 0;
     }
 }
@@ -415,7 +497,7 @@ void initialScreen()
         rno = login();
         if (rno == 0)
         {
-            // initialScreen();
+            initialScreen();
         }
     }
     else if (c == 2)
@@ -433,7 +515,95 @@ void initialScreen()
         dashboard(rno);
     }
 }
+void myAccount(int rno)
+{
+    string hobbies[] = {"basketball", "volleyball", "football", "swimming", "running", "cycling", "cricket", "chess", "Music", "Singing", "AI & machine learning", "web & app development", "computer programming", "Blockchain", "Video Games", "Traveling and exploring new places", "painting", "drawing", "photography", "graphic design", "Watching movies", "Watching series"};
 
+    sqlite3 *db;
+    int rc = sqlite3_open("student.db", &db); // student.db is the name of the database file
+
+    if (rc)
+    {
+        sqlite3_close(db);
+        cout << "Failed to open database" << endl;
+        sleep(1);
+        return;
+    }
+
+    const char *sql = "SELECT Name, Gender, RollNo, Dept, Email, Interest, Available FROM Students WHERE RollNo = ?";
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK)
+    {
+        sqlite3_close(db);
+        cout << "Failed to prepare statement" << endl;
+        sleep(1);
+        return;
+    }
+
+    sqlite3_bind_int(stmt, 1, rno);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW)
+    {
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        cout << "Failed to execute statement" << endl;
+        sleep(1);
+        return;
+    }
+
+    string name = string((const char *)sqlite3_column_text(stmt, 0));
+    bool gender = (sqlite3_column_int(stmt, 1) == 1) ? true : false;
+    int rollno = sqlite3_column_int(stmt, 2);
+    string dept = string((const char *)sqlite3_column_text(stmt, 3));
+    string email = string((const char *)sqlite3_column_text(stmt, 4));
+    string interests = string((const char *)sqlite3_column_text(stmt, 5));
+    int score = 0;
+    bool available = (sqlite3_column_int(stmt, 6) == 1) ? true : false;
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+#ifdef _WIN32
+    system("cls");
+#elif _WIN64
+    system("cls");
+#else
+    system("clear");
+#endif
+    cout << "\t\tMyAccount";
+    cout << "Name: " << name << endl;
+    cout << "Gender: " << (gender ? "Male" : "Female") << endl;
+    cout << "Roll No: " << rollno << endl;
+    cout << "Department: " << dept << endl;
+    cout << "Email: " << email << endl;
+    cout << "Interests: ";
+    for (int i = 0; i < interests.length(); i++)
+    {
+        if (interests[i] == '1')
+        {
+            cout << hobbies[i] << ", ";
+        }
+    }
+    cout << endl;
+
+    cout << "Availability: " << (available ? "Yes" : "No") << endl;
+
+    cout << "press 1 edit interest\npress 0 to exit:$";
+    string a;
+    cin >> a;
+    if (a == "1")
+    {
+        string newInterest = getInterest();
+        updateInterest(rno, newInterest);
+        cout << "updated";
+        sleep(1);
+    }
+    else
+    {
+        dashboard(rno);
+    }
+}
 void dashboard(int rollno)
 {
 #ifdef _WIN32
@@ -443,12 +613,196 @@ void dashboard(int rollno)
 #else
     system("clear");
 #endif
-    cout << "DASHBOARD \n\n1.My Account\n2.View match\n3.Signout";
+    cout << "\tDASHBOARD \n\n1.My Account\n2.View match\n3.Signout\n$";
     int choice;
     cin >> choice;
-    if (choice == 3)
+    if (choice == 1)
+    {
+        myAccount(rollno);
+    }
+    else if (choice == 2)
+    {
+        viewMatches(rollno);
+    }
+    else if (choice == 3)
     {
         initialScreen();
+    }
+}
+void viewMatches(int rollnum)
+{
+    vector<User> userList = getUsersFromDatabase();
+    scoreUsers(userList, rollnum);
+    string p;
+    
+}
+pair<string, bool> getUserInfoFromDatabase(int rollno)
+{
+    string interests;
+    bool gender;
+
+    // connect to database
+    sqlite3 *db;
+    if (sqlite3_open("student.db", &db) == SQLITE_OK)
+    {
+        // build query to retrieve user details
+        string query = "SELECT gender, interest FROM Students WHERE rollno = " + to_string(rollno);
+
+        // execute query and retrieve results
+        sqlite3_stmt *stmt;
+        if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+        {
+            if (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                // retrieve values from database row
+                gender = sqlite3_column_int(stmt, 0);
+                const char *interestsChars = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+                interests = interestsChars ? interestsChars : "";
+                interests.resize(strlen(interestsChars));
+            }
+            else
+            {
+                // handle error if user not found
+                cout << "User with roll number " << rollno << " not found in database." << endl;
+            }
+        }
+        else
+        {
+            // handle error if query execution fails
+            cout << "Error executing query: " << sqlite3_errmsg(db) << endl;
+        }
+
+        // finalize statement and close database
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+    }
+    else
+    {
+        // handle error if database connection fails
+        cout << "Error connecting to database: " << sqlite3_errmsg(db) << endl;
+    }
+
+    return make_pair(interests, gender);
+}
+
+vector<User> getUsersFromDatabase()
+{
+    vector<User> userList;
+
+    // connect to database
+    sqlite3 *db;
+    if (sqlite3_open("student.db", &db) == SQLITE_OK)
+    {
+        // build query to retrieve user details
+        string query = "SELECT name, gender, rollno, dept, email, interest, available FROM Students";
+
+        // execute query and retrieve results
+        sqlite3_stmt *stmt;
+        if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL) == SQLITE_OK)
+        {
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                // retrieve values from database row
+                string name = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
+                int gender = sqlite3_column_int(stmt, 1);
+                int rollno = sqlite3_column_int(stmt, 2);
+                string dept = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3));
+                string email = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 4));
+                string interests = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 5));
+                int available = sqlite3_column_int(stmt, 6);
+
+                // create User object with score = 0 and add to vector
+                User user(name, gender, rollno, dept, email, interests, 0, available);
+                userList.push_back(user);
+            }
+        }
+        else
+        {
+            // handle error if query execution fails
+            cout << "Error executing query: " << sqlite3_errmsg(db) << endl;
+        }
+
+        // finalize statement and close database
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+    }
+    else
+    {
+        // handle error if database connection fails
+        cout << "Error connecting to database: " << sqlite3_errmsg(db) << endl;
+    }
+
+    return userList;
+}
+
+// function to get the interest,gender of a user from database by giing rollnum
+void scoreUsers(vector<User> userList, int roll)
+{
+    vector<pair<int, string> > scoreList; // vector to store scores and corresponding user name
+    string iInterests;
+    int iGender;
+    // Get user info for the specified roll number
+    pair<string, int> user_info = getUserInfoFromDatabase(roll);
+    iInterests = get<0>(user_info);
+    iGender = get<1>(user_info);
+
+    for (int j = 0; j < userList.size(); j++) // compare with all other users after i
+    {
+
+        if (iGender == userList[j].getGender() && userList[j].isAvailable())
+        { // check if users have same gender and are available
+            int commonInterests = 0;
+            string jInterests = userList[j].getUserInterests();
+            for (int k = 0; k < 22; k++) // count number of common interests
+            {
+                ;
+                if (iInterests[k] == '1' && jInterests[k] == '1')
+                {
+                    
+                    commonInterests = commonInterests + 1;
+                }
+            }
+            if (commonInterests > 0) // if common interests are found, add score to both users and add the other user's name to the vector
+            {
+                userList[j].addScore(commonInterests);
+                scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + userList[j].getEmail() + ", " + to_string(userList[j].getScore()) + ")")); // add other user's name, email, and score to vector
+            }
+        }
+    }
+
+    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string> >()); // sort vector in descending order of score
+    ofstream outFile("matches.txt");
+    if (scoreList.size() == 1)
+    {
+        cout << "Sorry!....no matches\nreturning to dashboard in 3..2..1";
+        sleep(3);
+        dashboard(roll);
+    }
+
+
+    for (int i = 1; i < scoreList.size(); i++) // print results
+    {
+        cout << scoreList[i].second << endl;
+    }
+    if (outFile.is_open())
+    {
+        for (int i = 1; i < scoreList.size(); i++)
+        {
+            outFile << scoreList[i].second << endl;
+        }
+        outFile.close();
+        cout << "Results written to file: matches.txt" << endl;
+    }
+
+    
+    cout<<"\nPress 1 to go back";
+    string p;
+    cin>>p;
+    if(p=="1"){
+        dashboard(roll);
+    }
+    else{
+         dashboard(roll);
     }
 }
 int main()
