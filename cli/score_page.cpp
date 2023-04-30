27c89b78f2bab8f787b6b3f3d4ddbaf30fe08fbe
nithin-kasauzki <106121086@nitt.edu>
2023-04-30 18:49:16 +0530

    made changes in user cls

--- cli/score_page.cpp
+++ cli/score_page.cpp
@@ -39,6 +39,21 @@ public:
         return name;
     }
 
+    string getDept()
+    {
+        return dept;
+    }
+
+    int getRollno()
+    {
+        return rollno;
+    }
+
+    string getEmail()
+    {
+        return email;
+    }
+
     bool isAvailable() // public member function to access the 'available' variable
     {
         return available;
@@ -50,38 +65,38 @@ public:
     }
 };
 
-void scoreUsers(vector<User>& userList)
+void scoreUsers(vector<User> &userList)
 {
-    vector<pair<int, string> > scoreList; // vector to store scores and corresponding user name
+    vector<pair<int, string>> scoreList; // vector to store scores and corresponding user name
 
-    int i=0;
-    
-        for (int j = i + 1; j < userList.size(); j++) // compare with all other users after i
-        {
-            if (userList[i].hasSameGender(userList[j]) && userList[j].isAvailable()) { // check if users have same gender and are available
-                int commonInterests = 0;
-                string iInterests = userList[i].getUserInterests();
-                string jInterests = userList[j].getUserInterests();
+    int i = 0;
 
-                for (int k = 0; k < iInterests.length() && k < jInterests.length(); k++) // count number of common interests
+    for (int j = i + 1; j < userList.size(); j++) // compare with all other users after i
+    {
+        if (userList[i].hasSameGender(userList[j]) && userList[j].isAvailable())
+        { // check if users have same gender and are available
+            int commonInterests = 0;
+            string iInterests = userList[i].getUserInterests();
+            string jInterests = userList[j].getUserInterests();
+
+            for (int k = 0; k < iInterests.length() && k < jInterests.length(); k++) // count number of common interests
+            {
+                if (iInterests[k] == '1' && jInterests[k] == '1')
                 {
-                    if (iInterests[k] == '1' && jInterests[k] == '1')
-                    {
-                        commonInterests++;
-                    }
-                }
-
-                if (commonInterests > 0) // if common interests are found, add score to both users and add the other user's name to the vector
-                {
-                    userList[i].addScore(commonInterests);
-                    userList[j].addScore(commonInterests);
-                    scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + to_string(userList[j].getScore()) + ")")); // add other user's name and score to vector
+                    commonInterests++;
                 }
             }
-        }
-    
 
-    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string> >()); // sort vector in descending order of score
+            if (commonInterests > 0) // if common interests are found, add score to both users and add the other user's name to the vector
+            {
+                userList[i].addScore(commonInterests);
+                userList[j].addScore(commonInterests);
+                scoreList.push_back(make_pair(commonInterests, userList[j].getName() + " (" + to_string(userList[j].getScore()) + ")")); // add other user's name and score to vector
+            }
+        }
+    }
+
+    sort(scoreList.begin(), scoreList.end(), greater<pair<int, string>>()); // sort vector in descending order of score
 
     for (int i = 0; i < scoreList.size(); i++) // print results
     {
@@ -89,19 +104,15 @@ void scoreUsers(vector<User>& userList)
     }
 }
 
-
-
-
 int main()
 {
     vector<User> userList;
 
     // populate userList with data
 
-    User user3("nishaanth",1,3, "EC", "nitin@example.com", "1100101000", 0, true);
-    User user4("David",1,4, "CS", "david@example.com", "1111101110", 0, true);
-    User user5("Emily",0,5, "ME", "emily@example.com", "1100001101", 0, true);
-
+    User user3("nishaanth", 1, 3, "EC", "nitin@example.com", "1100101000", 0, true);
+    User user4("David", 1, 4, "CS", "david@example.com", "1111101110", 0, true);
+    User user5("Emily", 0, 5, "ME", "emily@example.com", "1100001101", 0, true);
 
     userList.push_back(user3);
     userList.push_back(user4);
